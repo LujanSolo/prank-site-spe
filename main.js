fbc2dfab1f1efc8b896463b11c981c3cdd0d76b8
LujanSolo <lujansolo@proton.me>
2024-05-21 17:02:53 -0700

    added centered modal to ALL LINKS button in streaming section. added data-attributes for modal toggle

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const linksBtn = document.getElementById('all-links-btn');
+const hiddenModal = document.getElementById('links');
 
+linksBtn.addEventListener('click', () => {
+  console.log('clicked');
+  hiddenModal.classList.display = 'flex';
+});
