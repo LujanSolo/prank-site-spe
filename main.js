cc740731c2b353d60f7d4800b8696b7757eca41e
LujanSolo <lujansolo@proton.me>
2024-05-21 17:13:14 -0700

    removed js data for now

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-const linksBtn = document.getElementById('all-links-btn');
-const hiddenModal = document.getElementById('links');
 
-linksBtn.addEventListener('click', () => {
-  console.log('clicked');
-  hiddenModal.classList.display = 'flex';
-});
+
