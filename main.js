3d880242176a1e38c1af7928e468503eddedfed8
LujanSolo <lujansolo@proton.me>
2024-05-27 16:19:40 -0700

    updated navbar with highlighted active links. updated html to correct active bootstrap attribute

--- main.js
+++ main.js
@@ -6,33 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
   links.forEach(link => {
     link.addEventListener('click', function (event) {
       // Remove active-link class from all links
-      links.forEach(l => l.classList.remove('active-link'));
+      links.forEach(l => l.classList.remove('active'));
 
       // Add active-link class to the clicked link
-      event.currentTarget.classList.add('active-link');
+      event.currentTarget.classList.add('active');
 
       // Close the offcanvas
       bsOffcanvas.hide();
     });
   });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  const offcanvas = document.getElementById('offcanvasDarkNavbar'); // Get the offcanvas element by ID
-  const links = document.querySelectorAll('.nav-link'); // Select all nav links with class .nav-link
-  const bsOffcanvas = new bootstrap.Offcanvas(offcanvas); // Initialize Bootstrap offcanvas
-
-
-  links.forEach(link => {
-    link.addEventListener('touchend', function(event) {
-      links.forEach(l => l.classList.remove('active-link'));
-
-    // Add active-link class to the clicked link
-    event.currentTarget.classList.add('active-link');
-
-    // Close the offcanvas
-    bsOffcanvas.hide();
-    })
-  })
-  
 });
