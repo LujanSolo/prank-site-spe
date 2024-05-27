a945e5620647c21c095eec8be126762bc7872d49
LujanSolo <lujansolo@proton.me>
2024-05-27 16:07:50 -0700

    updated offcanvas menu to account for touch events - test - may need to remove

--- main.js
+++ main.js
@@ -16,3 +16,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const offcanvas = document.getElementById('offcanvasDarkNavbar'); // Get the offcanvas element by ID
+  const links = document.querySelectorAll('.nav-link'); // Select all nav links with class .nav-link
+  const bsOffcanvas = new bootstrap.Offcanvas(offcanvas); // Initialize Bootstrap offcanvas
+
+
+  links.forEach(link => {
+    link.addEventListener('touchend', function(event) {
+      links.forEach(l => l.classList.remove('active-link'));
+
+    // Add active-link class to the clicked link
+    event.currentTarget.classList.add('active-link');
+
+    // Close the offcanvas
+    bsOffcanvas.hide();
+    })
+  })
+  
+});
